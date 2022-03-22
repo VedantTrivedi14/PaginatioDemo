b88c231baa6fbc46ebf6e440af61b80e28b2dab3
vedant trivedi <vedanttrivedi83065@gmail.com>
2022-03-22 21:44:15 +0530

    Initial commit

---
+++ app/src/main/java/com/tatvasoftassignment/pagingdemo/MainActivity.java
@@ -0,0 +1,90 @@
+package com.tatvasoftassignment.pagingdemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.os.Bundle;
+
+import com.tatvasoftassignment.pagingdemo.databinding.ActivityMainBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+   ActivityMainBinding binding;
+   ArrayList<MainData> dataArrayList = new ArrayList<>();
+   MainAdapter adapter;
+   int page = 1 , limit = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        adapter = new MainAdapter(this,dataArrayList);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setAdapter(adapter);
+
+        getData(page,limit);
+        binding.scrollView.setOnScrollChangeListener((NestedScrollView.OnScrollChangeListener) (v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
+            page++;
+            getData(page,limit);
+        });
+
+    }
+
+    private void getData(int page, int limit) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://picsum.photos/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+
+        MainInterface MainInterface = retrofit.create(MainInterface.class);
+        Call<String> call = MainInterface.STRING_CALL(page,limit);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(@NonNull Call<String> call, @NonNull Response<String> response) {
+                if(response.isSuccessful() && response.body() != null){
+                    try {
+                        JSONArray jsonArray = new JSONArray(response.body());
+                        parseRequest(jsonArray);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<String> call, @NonNull Throwable t) {
+
+            }
+        });
+    }
+
+    private void parseRequest(JSONArray jsonArray) throws JSONException {
+       for(int i=0;i<jsonArray.length();i++) {
+           JSONObject object = jsonArray.getJSONObject(i);
+           MainData data = new MainData();
+           data.setImage(object.getString("download_url"));
+           data.setName(object.getString("author"));
+           dataArrayList.add(data);
+
+           adapter = new MainAdapter(this,dataArrayList);
+           binding.recyclerView.setAdapter(adapter);
+       }
+
+    }
+}
---
+++ app/src/main/java/com/tatvasoftassignment/pagingdemo/MainAdapter.java
@@ -0,0 +1,55 @@
+package com.tatvasoftassignment.pagingdemo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.tatvasoftassignment.pagingdemo.databinding.ListItemBinding;
+
+import java.util.ArrayList;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder>{
+
+    Context context;
+    ArrayList<MainData> dataArrayList;
+    public MainAdapter( Context context, ArrayList<MainData> dataArrayList){
+            this.context = context;
+            this.dataArrayList = dataArrayList;
+    }
+
+    @NonNull
+    @Override
+    public MainAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ListItemBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),R.layout.list_item,parent,false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MainAdapter.ViewHolder holder, int position) {
+        MainData data = dataArrayList.get(position);
+        Glide.with(context).load(data.getImage()).diskCacheStrategy(DiskCacheStrategy.ALL).into(holder.binding.imageView);
+        holder.binding.textView.setText(data.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataArrayList.size();
+    }
+
+    protected static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ListItemBinding binding;
+        public ViewHolder(ListItemBinding binding ) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
